ece4eb7a1edb80ac1be0ebed04c2858abdada523
silvenlily <61719718+silvenlily@users.noreply.github.com>
2022-04-03 15:13:23 -0600

    Update template.impl.user.js

--- template.impl.user.js
+++ template.impl.user.js
@@ -14,7 +14,7 @@ if (window.top !== window.self) {
     window.addEventListener('load', () => {
         // Load the image
         const image = document.createElement("img");
-        image.src = "https://cdn.mirai.gg/tmp/dotted-place-template.png";
+        image.src = "https://raw.githubusercontent.com/Ender0112/Transgender-Flag-Template/main/place.png";
         image.onload = () => {
             image.style = `position: absolute; left: 0; top: 0; width: ${image.width/3}px; height: ${image.height/3}px; image-rendering: pixelated; z-index: 1`;
         };
