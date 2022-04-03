4eb5e02e74b885798b5990a0522cf65e3f27a03e
silvenlily <61719718+silvenlily@users.noreply.github.com>
2022-04-03 14:35:36 -0600

    Update template.impl.user.js

--- template.impl.user.js
+++ template.impl.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Transgender Flag Template
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the canvas!
 // @author       oralekin - script, Ender#5769 - image
 // @match        https://hot-potato.reddit.com/embed*
