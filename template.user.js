9627438516746f84ea7d780e9f0faf16cb90c3f4
silvenlily <61719718+silvenlily@users.noreply.github.com>
2022-04-03 13:42:26 -0600

    Update and rename template.impl.user.js to template.user.js

---
+++ template.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Transgender Flag Template
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the canvas!
+// @author       oralekin - script, Ender#5769 - image
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://raw.githubusercontent.com/Ender0112/Transgender-Flag-Template/main/place.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
+            console.log(i);
+            return i;
+        })())
+
+    }, false);
+
+}
